8cc594e330903950a89882e8006694e0eaf1450e
CBTE Account <cbte@ac.sdmesa.net>
2020-02-05 11:50:38 -0800

    Lesson 1B

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 20;
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, 7);
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Obligatory comment
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
